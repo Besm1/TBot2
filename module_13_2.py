a7c79753ff41d1dcf33e0c04e275804f02fe33a2
b.smirnov <besm1@yandex.ru>
2024-11-05 23:17:21 +0300

    module_13_2.py - message.answer

---
+++ module_13_2.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+
+api = '---'
+bot = Bot(token=api)
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот, помогающий твоему здоровью.')
+
+@dp.message_handler(lambda message: message.text and 'hello' in message.text.lower()
+                                    or 'привет' in message.text.lower())
+async def greeting_messages(message):
+    print(f'Здор`ово, коли не шутишь.')
+
+@dp.message_handler()
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
